67e19c6f02247222bc26b36865c8542bcd9ef4d4
simw <simw@users.noreply.github.com>
2024-06-04 11:38:55 +0100

    Fix test_typing for different python versions

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -4,6 +4,7 @@ get_origin and get_args on some types. This behavior is relied
 on when examining the type annotations of a field in a model.
 """
 
+import sys
 import types
 from typing import Iterable, List, Literal, Optional, Union, get_args, get_origin
 
@@ -44,27 +45,32 @@ def test_list_no_type() -> None:
 
 
 def test_list() -> None:
-    t = list[str]
+    if sys.version_info >= (3, 9):
+        t = list[str]
 
-    assert t == list[str]
-    assert t != list
+        assert t == list[str]
+        assert t != list
 
-    assert not isinstance(t, type)
-    assert not issubclass(t, list)
-    with pytest.raises(TypeError):
-        issubclass(t, Iterable)
+        if sys.version_info < (3, 11):
+            assert isinstance(t, type)
+        else:
+            assert not isinstance(t, type)
+        assert not issubclass(t, list)
+        with pytest.raises(TypeError):
+            issubclass(t, Iterable)
 
-    assert get_origin(t) == list
-    assert issubclass(get_origin(t), Iterable)
+        assert get_origin(t) == list
+        assert issubclass(get_origin(t), Iterable)
 
-    assert get_args(t) == (str,)
+        assert get_args(t) == (str,)
 
 
 def test_typing_list() -> None:
     t = List[str]
 
     assert t == List[str]
-    assert t != list[str]
+    if sys.version_info >= (3, 9):
+        assert t != list[str]
     assert t != List
     assert t != list
 
@@ -111,30 +117,35 @@ def test_optional() -> None:
     1. str | None
     2. Optional[str]
     3. Union[str, None]
+
+    2. and 3. are very similar, except that the ordering of 3. could
+    be reversed by the user (ie Union[None, str] vs Union[str, None])
     """
-    assert not isinstance(str | None, type)
-    with pytest.raises(TypeError):
-        issubclass(str | None, str)  # type: ignore
+    if sys.version_info >= (3, 10):
+        assert not isinstance(str | None, type)
+        with pytest.raises(TypeError):
+            issubclass(str | None, str)  # type: ignore
+        assert get_origin(str | None) == types.UnionType
+        assert isinstance(get_origin(str | None), type)
+        assert not issubclass(get_origin(str | None), Iterable)
+        assert get_args(str | None) == (str, type(None))
+        assert get_args(None | str) == (type(None), str)
+
     assert not isinstance(Optional[str], type)
     with pytest.raises(TypeError):
         issubclass(Optional[str], str)  # type: ignore
-    assert not isinstance(Union[str, None], type)
-    with pytest.raises(TypeError):
-        issubclass(Union[str, None], str)  # type: ignore
-
-    assert get_origin(str | None) == types.UnionType
-    assert get_origin(Union[str, None]) == Union
     assert get_origin(Optional[str]) == Union
-    assert isinstance(get_origin(str | None), type)
-    assert not issubclass(get_origin(str | None), Iterable)
-    assert not isinstance(get_origin(Union[str, None]), type)
-    with pytest.raises(TypeError):
-        issubclass(get_origin(Union[str, None]), Iterable)  # type: ignore
     assert not isinstance(get_origin(Optional[str]), type)
     with pytest.raises(TypeError):
         issubclass(get_origin(Optional[str]), Iterable)  # type: ignore
+    assert get_args(Optional[str]) == (str, type(None))
 
-    assert get_args(str | None) == (str, types.NoneType)
-    assert get_args(None | str) == (types.NoneType, str)
-    assert get_args(Optional[str]) == (str, types.NoneType)
-    assert get_args(Union[None, str]) == (types.NoneType, str)
+    assert not isinstance(Union[str, None], type)
+    with pytest.raises(TypeError):
+        issubclass(Union[str, None], str)  # type: ignore
+    assert get_origin(Union[str, None]) == Union
+    assert not isinstance(get_origin(Union[str, None]), type)
+    with pytest.raises(TypeError):
+        issubclass(get_origin(Union[str, None]), Iterable)  # type: ignore
+    assert get_args(Union[str, None]) == (str, type(None))
+    assert get_args(Union[None, str]) == (type(None), str)
